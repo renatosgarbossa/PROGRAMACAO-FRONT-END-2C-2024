22bb4717e3456ccf6060a2107e8ccf17cb6e3ece
Renato Sgarbossa Iesque <125359611+renatosgarbossa@users.noreply.github.com>
2025-02-26 10:22:50 -0300

    Add files via upload

---
+++ My project/script.js
@@ -0,0 +1,9 @@
+function showSignUpForm() {
+    document.getElementById("loginSection").style.display = "none";
+    document.getElementById("signupSection").style.display = "block";
+}
+
+function showLoginForm() {
+    document.getElementById("signupSection").style.display = "none";
+    document.getElementById("loginSection").style.display = "block";
+}
